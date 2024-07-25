08a3fcd5cf33581b7a8fdfb82a9c96a62e61aafe
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-07-25 18:47:51 -0400

    modified login/signup styles

--- angular-express-app/src/app/modules/auth/login-signup/login-signup.component.ts
+++ angular-express-app/src/app/modules/auth/login-signup/login-signup.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabGroupComponent } from "./tab-group/tab-group.component";
-import { TabContentComponent } from "./tab-content/tab-content.component";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
+import { LoginFormComponent } from "./login-form/login-form.component";
 
 @Component({
   selector: 'app-login-signup',
   standalone: true,
-  imports: [RouterOutlet, TabGroupComponent, TabContentComponent],
+  imports: [RouterOutlet, TabGroupComponent, SignupFormComponent, LoginFormComponent],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.scss'
 })
 export class LoginSignupComponent {
+  activeTab: string = 'signup'; // Default active tab
+
+  onTabSelected(tab: string) {
+    this.activeTab = tab;
+  }
 
 }
