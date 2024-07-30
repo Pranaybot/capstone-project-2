395b07ad4334bb0d5c7716337b86be22d7b386b7
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-07-30 08:48:09 -0400

    Creates tab-group.component.ts file

---
+++ angular-express-app/src/app/modules/auth/login-signup/tab-group/tab-group.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-group.component.html',
+  styleUrl: './tab-group.component.scss'
+})
+export class TabGroupComponent {
+  @Input() activeTab: string;
+  @Output() tabSelected = new EventEmitter<string>();
+
+  onTabSelected(tab: string) {
+    this.tabSelected.emit(tab);
+  }
+
+}
