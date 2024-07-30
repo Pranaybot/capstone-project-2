793030a88694d287684f730f8462e687b73456db
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-07-29 22:48:20 -0400

    fixed tab content issue with login signup page

--- angular-express-app/src/app/modules/auth/login-signup/login-signup.component.ts
+++ angular-express-app/src/app/modules/auth/login-signup/login-signup.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Import CommonModule
 
 @Component({
   selector: 'app-login-signup',
   standalone: true,
   imports: [
     RouterOutlet,
+    CommonModule
   ],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.scss'
