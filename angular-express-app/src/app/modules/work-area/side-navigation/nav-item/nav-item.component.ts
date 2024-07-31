35d4e772982e7df559b7276456b2267b5515ccc1
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-07-31 08:51:32 -0400

    assigned default values to inputs in nav-item.component.ts

--- angular-express-app/src/app/modules/work-area/side-navigation/nav-item/nav-item.component.ts
+++ angular-express-app/src/app/modules/work-area/side-navigation/nav-item/nav-item.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   standalone: true
 })
 export class NavItemComponent {
-  @Input() icon: string;
-  @Input() label: string;
-  @Input() type: string;
+  @Input() icon: string = '';
+  @Input() label: string = '';
+  @Input() type: string = '';
 }
