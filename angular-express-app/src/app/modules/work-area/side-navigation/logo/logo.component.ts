735435721b48f2c5f93b031c98aab42220574455
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-07-31 11:27:25 -0400

    imported othere elements from angular/core library in logo.component.ts

--- angular-express-app/src/app/modules/work-area/side-navigation/logo/logo.component.ts
+++ angular-express-app/src/app/modules/work-area/side-navigation/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { DOMService } from '../../../../../services/dom.service';
 
 @Component({
@@ -9,33 +9,33 @@ import { DOMService } from '../../../../../services/dom.service';
   styleUrl: './logo.component.scss'
 })
 export class LogoComponent 
-  implements OnInit, AfterViewInit{
-
-    constructor(private domService: DomService) { }
+  implements OnInit {
+      @ViewChild('btn', {static: 
+    false}) btn: ElementRef |undefined;
+    
+    constructor(
+      private renderer: Renderer2, 
+      private domService: DomService) { }
 
     ngOnInit(): void {
       this.addDynamicScript();
     }
 
     addDynamicScript() {
-      const script = document.createElement('script');
+      const script = this.renderer.createElement('script');
       script.type = 'text/javascript';
       script.text = `
         document.addEventListener('DOMContentLoaded', () => {
-          let closeBtn = document.querySelector('#btn')});`;
-
-      document.body.appendChild(script);
+          let closeBtn = document.querySelector('#btn')});
+      `;
+      
+      this.renderer.appendChild(document.body, script);
     }
 
     ngAfterViewInit() {
-      setTimeout(() => {
-        const closeBtn = document.querySelector('#btn');
-
-        if (closeBtn) {
-          this.domService.setCloseBtn(closeBtn);
-        }
-      }, 0);
-
+      if (this.btn) {
+        this.domService.setCloseBtn(this.btn.nativeElement);
+      }
     }
           
 }
