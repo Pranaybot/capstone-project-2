62d7e372c5170418b679146026534017be9ab14b
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-07-31 10:26:07 -0400

    Update logic for handling closeBtn DOM element in logo.component.ts

--- angular-express-app/src/app/modules/work-area/side-navigation/logo/logo.component.ts
+++ angular-express-app/src/app/modules/work-area/side-navigation/logo/logo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DOMService } from '../../../../../services/dom.service';
 
 @Component({
   selector: 'app-logo',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss'
 })
-export class LogoComponent {
+export class LogoComponent 
+  implements OnInit, AfterViewInit{
 
+    constructor(private domService: DomService) { }
+
+    ngOnInit(): void {
+      this.addDynamicScript();
+    }
+
+    addDynamicScript() {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.text = `
+        document.addEventListener('DOMContentLoaded', () => {
+          let closeBtn = document.querySelector('#btn')});`;
+
+      document.body.appendChild(script);
+    }
+
+    ngAfterViewInit() {
+      setTimeout(() => {
+        const closeBtn = document.querySelector('#btn');
+
+        if (closeBtn) {
+          this.domService.setCloseBtn(closeBtn);
+        }
+      }, 0);
+
+    }
+          
 }
