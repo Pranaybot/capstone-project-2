dc6f1234d1a4b2958e6f9d1f88b95460bcece05d
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 08:29:23 -0400

    Create list.service.ts

---
+++ angular-express-app/src/app/services/list.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from "../services/base.service"
+import { List } from '../shared/models/list';
+import { Card } from '../shared/models/card';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService extends BaseService {
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  get_all_lists(): Observable<List[]> {
+    return this.http.get<List[]>(`${this.baseUrl}/list/`);
+  }
+
+  add_list(name: string, cards: any): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/list/add_list`, { name, cards });
+  }
+
+  update_list(id: string, name: string): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/list/update_list`, { id, name });
+  }
+
+  delete_list(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/list/${id}`);
+  }
+
+  add_card(listId: string, card: Card): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/list/${listId}/cards`, card);
+  }
+
+}
