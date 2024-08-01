0d5f99562157e435339dbbf6225a6c3d024f21a0
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 11:18:46 -0400

    imported initializedatabse method from db.ts in dbRoutes.ts

--- backend/utils/route_handlers/dbRoutes.ts
+++ backend/utils/route_handlers/dbRoutes.ts
@@ -1,9 +1,19 @@
-// DbRoutes.ts
+// dbRoutes.ts
 import { Application } from 'express';
+import initializeDatabase from '../../database/db';
 import userRoute from '../../routes/userRoute';
 import listRoute from '../../routes/listRoute';
 
 export function setupDbRoutes(app: Application) {
-  app.use('/user', userRoute);
-  app.use('/list', listRoute);
+  initializeDatabase()
+    .then(() => {
+      console.log("Cassandra DB initialized");
+
+      // Register routes that depend on the database
+      app.use('/user', userRoute);
+      app.use('/list', listRoute);
+    })
+    .catch((err: any) => {
+      console.error("Failed to initialize cassankub", err);
+    });
 }
