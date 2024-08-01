20a2acbe71d5162c501ab074c7dcaa7dc78f0ae9
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 11:50:21 -0400

    Create login-form.service.ts

---
+++ angular-express-app/src/app/services/login-form.service.ts
@@ -0,0 +1,20 @@
+
+import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginFormService {
+  constructor(private fb: FormBuilder) {}
+
+  createLoginForm(): FormGroup {
+    return this.fb.group({
+      username: ['', 
+        [Validators.required,
+         Validators.email]],
+      pwd: ['',
+        Validators.required]
+    });
+  }
+}
