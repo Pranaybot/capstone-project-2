c7e4f4217b053f93caf314010487ea547fe4e383
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 13:10:22 -0400

    commented loginMiddleware.ts

--- backend/middleware/loginMiddleware.ts
+++ backend/middleware/loginMiddleware.ts
@@ -1,6 +1,8 @@
+/*
 import createBaseMiddleware from './baseMiddleware';
 import { validateLogin } from '../utils/input_validaton/userLogInValidation';
 
 const loginMiddleware = createBaseMiddleware(validateLogin);
 
 export default loginMiddleware;
+*/
