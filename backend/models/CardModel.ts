ae9833428c10c4f8dac4f68faed6d8bb5e338a23
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 10:58:04 -0400

    Create CardModel.ts

---
+++ backend/models/CardModel.ts
@@ -0,0 +1,9 @@
+export const createCardTableQuery = `
+  CREATE TABLE IF NOT EXISTS cards (
+    id uuid PRIMARY KEY,
+    username text,
+    title text,
+    description text,
+    activity text
+  )
+`;
