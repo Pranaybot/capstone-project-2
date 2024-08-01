309f4f1c5245a714ca031c98e5534beafc59863f
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 10:26:08 -0400

    Create workspace-title.component.ts

---
+++ angular-express-app/src/app/modules/work-area/workspace/workspace-title/workspace-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-title',
+  templateUrl: './workspace-title.component.html',
+  styleUrl: './workspace-title.component.scss'
+})
+export class WorkspaceTitleComponent {
+
+}
