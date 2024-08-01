3deba34c90aba7f3d0c9116bbdb57903c1590b27
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 09:29:15 -0400

    added imports and updated logic to login form handling in login-form.component.ts

--- angular-express-app/src/app/modules/auth/login-signup/login-form/login-form.component.ts
+++ angular-express-app/src/app/modules/auth/login-signup/login-form/login-form.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginHandler } from '../../../../shared/handlers/login-handler';  // Import the SignupHandler class
+import { ReactiveFormsModule, FormsBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
-export class LoginFormComponent {}
+export class LoginFormComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private loginHandler: LoginHandler, // Inject LoginHandler
+    private fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.loginForm = 
+      this.fb.group({
+        username: ['',
+          [Validators.required,
+           Validators.email]],
+        pwd: ['' , 
+          Validators.required]
+      });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.loginHandler.handleLogin(this.loginForm); // Delegate login handling to LoginHandler
+    }
+  }
+  
+}
 
