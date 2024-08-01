6fb1a761bcac1c5374966dce21699eedb68341f4
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 10:47:37 -0400

    Create cassandraConfig.ts

---
+++ backend/config/cassandraConfig.ts
@@ -0,0 +1,12 @@
+
+const config = {
+    "contactPoints": ["127.1.1.23"],
+    "localDataCenter": "center1",
+    "keyspace": "",
+    "credentials":{
+        "username": "",
+        "password": ""
+    }
+};
+
+module.exports = config;
