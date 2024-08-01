7851e621aaa6beb7aefeb50624b1c92584279efb
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 08:25:17 -0400

    Delete angular-express-app/src/app/models directory

--- angular-express-app/src/app/models/list.ts
+++
@@ -1,7 +0,0 @@
-import { Card } from './card'; // Adjust the import path based on your project structure
-
-export interface List {
-  id?: string;
-  name: string;
-  cards?: Card[]; // Add cards property as an optional array of Card objects
-}
