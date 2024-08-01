265e4c33fd82a037c8fff7479ae9f53887d23019
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 13:12:36 -0400

    commented middleware and db routes imports in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 //server.ts
 import express from "express";
 import { configureApp } from "../../config/appConfig";
+/*
 import { setupMiddleware } from "../../utils/route_handlers/appMiddleware";
-import { setupDbRoutes } from '../../utils/route_handlers/dbRoutes'; // Adjust the path as needed
+import { setupDbRoutes } from '../../utils/route_handlers/dbRoutes';
+*/
 import { startServer } from "../../config/serverConfig";
 
 const app = express();
@@ -10,11 +12,13 @@ const app = express();
 // Configure app settings
 configureApp(app);
 
+/*
 // Setup middleware
 setupMiddleware(app);
 
 // Setup routes that require database access
 setupDbRoutes(app);
+*/
 
 // Starts angular express application
 startServer(app);
