650344b19beed65fbc45f78375c59486fa1ed05b
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 13:13:35 -0400

    commented userLogInValidation.ts

--- backend/utils/input_validation/userLogInValidation.ts
+++ backend/utils/input_validation/userLogInValidation.ts
@@ -1,3 +1,4 @@
+/*
 import { body } from 'express-validator';
 
 export const validateLogin = () => {
@@ -10,3 +11,4 @@ export const validateLogin = () => {
       .withMessage('Password is required'),
   ];
 };
+*/
