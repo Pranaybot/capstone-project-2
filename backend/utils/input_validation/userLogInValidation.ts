7f679efcb04c07dc198b4cc1fa0f6590c8aa49ee
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 11:03:26 -0400

    Create userLogInValidation.ts

---
+++ backend/utils/input_validation/userLogInValidation.ts
@@ -0,0 +1,12 @@
+import { body } from 'express-validator';
+
+export const validateLogin = () => {
+  return [
+    body('username')
+      .isEmail()
+      .withMessage('Invalid email format for username'),
+    body('pwd')
+      .notEmpty()
+      .withMessage('Password is required'),
+  ];
+};
