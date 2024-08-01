2e6fec579c1c1ccd9fa05255d031cf1e75cb7b25
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 11:23:18 -0400

    commented code for list and card models in db.ts file

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,18 +1,22 @@
 import { createUserTableQuery } from "../models/UserModel";
+/*
 import { createListTableQuery } from "../models/listModel";
 import { createCardTableQuery } from "../models/CardModel";
 import { createCardTypeQuery } from "../utils/user_defined_types/cardType";
+*/
 import client from "../config/clientConfig";
 
 async function initializeDatabase() {
     try {
         // Execute table creation queries
         await client.execute(createUserTableQuery);
+        /*
         await client.execute(createListTableQuery);
         await client.execute(createCardTableQuery);
 
         // Execute type creation queries
         await client.execute(createCardTypeQuery);
+        */
         
         console.log('Database initialized successfully');
     } catch (error) {
