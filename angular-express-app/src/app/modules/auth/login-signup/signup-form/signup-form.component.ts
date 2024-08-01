32fb54c14bf23c96e82e687a8c868d4a013b58da
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 09:23:31 -0400

    created ngOnInit() method in signup-form.component.ts

--- angular-express-app/src/app/modules/auth/login-signup/signup-form/signup-form.component.ts
+++ angular-express-app/src/app/modules/auth/login-signup/signup-form/signup-form.component.ts
@@ -11,20 +11,32 @@ import { ReactiveFormsModule, FormsBuilder, FormGroup, Validators } from '@angul
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.scss'
 })
-export class SignupFormComponent {
-  
-   signupData = {
-    firstname: '',
-    lastname: '',
-    username: '',
-    pwd: ''
-  };
+export class SignupFormComponent implements OnInit {
+  signupForm: FormGroup;
 
   constructor(
-    private signupHandler: SignupHandler) {}
+    private signupHandler: SignupHandler,
+    private fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.signupForm = 
+      this.fb.group({
+        firstname: ['',
+          Validators.required],
+        lastname: ['',
+          Validators.required],
+        username: ['',
+          [Validators.required,
+           Validators.email]],
+        pwd: ['' , 
+          Validators.required]
+      });
+  }
 
   signup() {
-    this.signupHandler.handleSignup(this.signupData); // Delegate signup handling to SignupHandler
+    if (this.signupForm.valid) {
+      this.signupHandler.handleSignup(this.signupData); // Delegate signup handling to SignupHandler
+    }
   }
 
 }
