43eb509af4a4f303e200384009acc9197d4e0d25
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 13:08:48 -0400

    Update clientConfig.ts

--- backend/config/clientConfig.ts
+++ backend/config/clientConfig.ts
@@ -1,6 +1,8 @@
+/*
 import casssandra from "cassandra-driver";
 const { config } = require("../config/cassandraConfig");
 
 const client = new casssandra.Client(config)
 
 export default client;
+*/
