5085b99c5c234193db6c32ef1477408cbe812664
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 11:05:07 -0400

    Delete backend/utils/input_validation/queries directory

--- backend/utils/input_validation/queries/user.ts
+++
@@ -1,9 +0,0 @@
-const SELECT_USER_BY_ID = 'SELECT * FROM users WHERE id = ?';
-const SELECT_USER_BY_USERNAME = 'SELECT * FROM users WHERE username = ?';
-const INSERT_USER = 'INSERT INTO users (id, firstName, lastName, userId, password) VALUES (?, ?, ?, ?, ?)';
-
-export default {
-  SELECT_USER_BY_ID,
-  SELECT_USER_BY_USERNAME,
-  INSERT_USER,
-};
