89f8dfad28795296f3b981765cdb1e948dfb5811
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 08:30:39 -0400

    Create card.service.ts

---
+++ angular-express-app/src/app/services/card.service.ts
@@ -0,0 +1,23 @@
+// src/app/services/card.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseService } from './base.service';
+import { Card } from '../shared/models/card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService extends BaseService {
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  update_card(cardId: string, card: Card): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/list/cards/${cardId}`, card);
+  }
+
+  delete_card(listId: string, cardId: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/list/${listId}/cards/${cardId}`);
+  }
+}
