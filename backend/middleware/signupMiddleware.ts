bcc885f46beba8c712411ac3546fff4e2f7b3efe
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-01 13:10:38 -0400

    commented signupMiddleware.ts

--- backend/middleware/signupMiddleware.ts
+++ backend/middleware/signupMiddleware.ts
@@ -1,6 +1,8 @@
+/*
 import createBaseMiddleware from './baseMiddleware';
 import { validateSignup } from '../utils/input_validaton/userSignUpValidation';
 
 const signupMiddleware = createBaseMiddleware(validateSignup);
 
 export default signupMiddleware;
+*/
