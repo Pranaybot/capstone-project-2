a4559d49a78c11c06729f9df7fe672eb99ee878b
Pranay Pentaparthy <pranaypentaparthy@pranays-mbp.lan>
2024-08-03 15:30:09 -0400

    Fixed issue with side-navigation component design

--- angular-express-app/src/app/modules/work-area/side-navigation/side-navigation.component.ts
+++ angular-express-app/src/app/modules/work-area/side-navigation/side-navigation.component.ts
@@ -1,53 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { LogoComponent } from './logo/logo.component';
-import { NavItemComponent } from './nav-item/nav-item.component';
-import { DomService } from '../../../services/dom.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-navigation',
   standalone: true,
-  imports: [
-    LogoComponent,
-    NavItemComponent
-  ],
+  imports: [],
   templateUrl: './side-navigation.component.html',
   styleUrl: './side-navigation.component.scss'
 })
-export class SideNavigationComponent implements OnInit{
-  constructor(
-    private domService: DomService) { }
-  
-    ngOnInit(): void {
-      const closeBtn = this.domService.getCloseBtn();
-      if (closeBtn) {
-        const scriptContent = `
-        document.addEventListener('DOMContentLoaded', () => {
-          let sidebar = document.querySelector(".sidebar");
-          let searchBtn = document.querySelector(".bx-search");
-          
-          closeBtn.addEventListener("click", ()=>{
-            sidebar.classList.toggle("open");
-            menuBtnChange();//calling the function(optional)
-          });
-          
-          searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-            sidebar.classList.toggle("open");
-            menuBtnChange(); //calling the function(optional)
-          });
-          
-          // following are the code to change sidebar button(optional)
-          function menuBtnChange() {
-          if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-          }else {
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-          }
-          }
-        });
-        `;
-        
-        this.domService.addDynamicScript(scriptContent);
-      }
-    }
+export class SideNavigationComponent {
 
 }
