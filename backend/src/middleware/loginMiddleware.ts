4267664612ff74500e5f0e9293c8e2592bfba3f1
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-09 09:14:24 -0400

    Delete backend/src/middleware directory

--- backend/src/middleware/loginMiddleware.ts
+++
@@ -1,8 +0,0 @@
-
-import createBaseMiddleware from './baseMiddleware';
-import { validateLogin } from '../utils/input_validation/userLogInValidation';
-
-const loginMiddleware = createBaseMiddleware(validateLogin);
-
-export default loginMiddleware;
-
--- backend/src/middleware/signupMiddleware.ts
+++
@@ -1,8 +0,0 @@
-
-import createBaseMiddleware from './baseMiddleware';
-import { validateSignup } from '../utils/input_validation/userSignUpValidation';
-
-const signupMiddleware = createBaseMiddleware(validateSignup);
-
-export default signupMiddleware;
-
