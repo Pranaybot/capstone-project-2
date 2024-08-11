1443d07839d4094dd08935f4ef142d1a2b047c43
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-08-10 23:49:12 -0400

    formatted quotation marks for user queries with tick marks

--- backend/src/utils/queries/user.ts
+++ backend/src/utils/queries/user.ts
@@ -1,7 +1,7 @@
 
-const SELECT_USER_BY_ID = 'SELECT * FROM users WHERE id = ?';
-const SELECT_USER_BY_USERNAME = 'SELECT * FROM users WHERE username = ?';
-const INSERT_USER = 'INSERT INTO users (id, firstName, lastName, userId, password) VALUES (?, ?, ?, ?, ?)';
+const SELECT_USER_BY_ID = `SELECT * FROM users WHERE id = ?`;
+const SELECT_USER_BY_USERNAME = `SELECT * FROM users WHERE userId = ?`;
+const INSERT_USER = `INSERT INTO users (id, firstName, lastName, userId, password) VALUES (?, ?, ?, ?, ?)`;
 
 export default {
   SELECT_USER_BY_ID,
