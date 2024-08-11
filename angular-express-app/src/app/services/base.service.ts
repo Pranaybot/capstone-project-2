c20f61b4f41cbda644fd4babb936e4ddd663ce67
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-08-11 09:09:11 -0400

    added proxy.conf json file to angular json file

--- angular-express-app/src/app/services/base.service.ts
+++ angular-express-app/src/app/services/base.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 
 export class BaseService {
-  protected apiUrl: string = 'http://localhost:3000';
+  protected apiUrl: string = '/api';
 
   constructor(protected http: HttpClient) {}
 
