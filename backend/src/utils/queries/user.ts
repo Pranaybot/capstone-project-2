36099e0e5f42965ddebc2d02c40cea18fc384d9b
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-08-11 00:50:31 -0400

    added  to user file in queries folder

--- backend/src/utils/queries/user.ts
+++ backend/src/utils/queries/user.ts
@@ -1,6 +1,6 @@
 
-const SELECT_USER_BY_ID = `SELECT * FROM users WHERE id = ?`;
-const SELECT_USER_BY_USERNAME = `SELECT * FROM users WHERE userId = ?`;
+const SELECT_USER_BY_ID = `SELECT * FROM users WHERE id = ? ALLOW FILTERING`;
+const SELECT_USER_BY_USERNAME = `SELECT * FROM users WHERE userId = ? ALLOW FILTERING`;
 const INSERT_USER = `INSERT INTO users (id, firstName, lastName, userId, password) VALUES (?, ?, ?, ?, ?)`;
 
 export default {
