3fc95ad83b8c57737a3f9a0543543d82526c4b6e
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-13 12:32:35 -0400

    removed OnInit method from navbar component and modified logout button check conditions in navbar html file

--- angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
+++ angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { AuthService } from '../../../services/auth.service';
 import { UserService } from '../../../services/user.service';
 
 @Component({
@@ -10,27 +9,11 @@ import { UserService } from '../../../services/user.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  showLogOutButton: boolean = false;
+export class NavbarComponent {
 
   constructor(public userservice: UserService) {}
 
-  ngOnInit(): void {
-    this.checkLogoutButtonVisibility();
-  }
-
-  checkLogoutButtonVisibility() {
-    if (this.userservice.isLoggedIn 
-      && !this.userservice.isHome) {
-      this.showLogOutButton = true;
-    } else if (!this.userservice.isLoggedIn
-      && this.userservice.isHome) {
-      this.showLogOutButton = false;
-    } 
-  }
-
   onLogout() {
     this.userservice.logout();
-    this.checkLogoutButtonVisibility();
   }
 }
