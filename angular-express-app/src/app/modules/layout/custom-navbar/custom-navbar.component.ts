38c9512397b0f5b60104d97d7883937340fb7e8b
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-08-18 09:46:58 -0400

    added custom navbar component to custom component to fix background color issue for login, signup, and reset password pages

---
+++ angular-express-app/src/app/modules/layout/custom-navbar/custom-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-navbar',
+  standalone: true,
+  imports: [],
+  styleUrl: './custom-navbar.component.scss',
+  templateUrl: './custom-navbar.component.html',
+})
+export class CustomNavbarComponent {
+
+}
--- angular-express-app/src/app/modules/layout/custom/custom.component.ts
+++ angular-express-app/src/app/modules/layout/custom/custom.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CustomNavbarComponent } from '../custom-navbar/custom-navbar.component';
 
 @Component({
   selector: 'app-custom',
   template: `
+    <app-custom-navbar></app-custom-navbar>
     <router-outlet></router-outlet>
   `,
   standalone: true,
   imports: [
     CommonModule, 
-    RouterModule
+    RouterModule,
+    CustomNavbarComponent
   ]
 })
 export class CustomComponent {}
