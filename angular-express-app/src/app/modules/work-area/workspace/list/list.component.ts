8c0b7304f82d06e58450fdfc69492be10320a865
Pranay Pentaparthy <pranaypentaparthy@Pranays-MBP.attlocal.net>
2024-08-19 16:41:52 -0400

    updated list component methods with card and list service methods

--- angular-express-app/src/app/modules/work-area/workspace/list/list.component.ts
+++ angular-express-app/src/app/modules/work-area/workspace/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ListService } from "../../../../services/work-area/list.service";
+import { CardService } from '../../../../services/work-area/card.service';
 import { List } from "../../../../shared/models/list";
 import { Card } from "../../../../shared/models/card";
 import { CommonModule } from '@angular/common';
@@ -20,12 +21,11 @@ import { CardComponent } from "./card/card.component";
 export class ListComponent implements OnInit {
   @Input() lists: List[] = [];
   @Input() maxLists: number = 10;
-  @Output() updateList = new EventEmitter<{ id: string, name: string }>();
-  @Output() deleteList = new EventEmitter<string>();
-  @Output() addCard = new EventEmitter<{ listId: string, card: Card }>();
-  @Output() addList = new EventEmitter<{ name: string, cards: Card[] }>();
 
-  constructor(private listService: ListService) {}
+  constructor(
+    private listService: ListService,
+    private cardService: CardService
+  ) {}
 
   ngOnInit(): void {
     // Only add default lists if the list array is empty
@@ -48,28 +48,28 @@ export class ListComponent implements OnInit {
 
   updateListName(list: List): void {
     if (list && list.id && list.name) {
-      this.updateList.emit({ id: list.id, name: list.name });
+      this.listService.update_list(list.id, list.name);
     }
   }
 
   deleteListItem(id: string | undefined): void {
     if (id) {
-      this.deleteList.emit(id);
+      this.listService.delete_list(id);
     }
   }
 
   addCardToList(listId: string): void {
     if (listId) {
-      this.addCard.emit({
-        listId: listId,
-        card: { username: '', title: 'New Card', description: '', activity: '' }
-      });
+      this.cardService.add_card(
+        listId, 
+        {username: '', title: 'New Card', description: '', activity: ''}
+      )
     }
   }
 
   addNewList(name: string): void {
     if (this.lists.length < this.maxLists) {
-      this.addList.emit({ name: name, cards: [] });
+      this.listService.add_list(name, []);
     }
   }
 }
