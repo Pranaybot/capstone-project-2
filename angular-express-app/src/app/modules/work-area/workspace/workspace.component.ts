e6483a9d497398c857e652b60214401aca4991fc
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-21 18:44:53 -0400

    added ngOniit method in workspace component file to detect changes in list component

--- angular-express-app/src/app/modules/work-area/workspace/workspace.component.ts
+++ angular-express-app/src/app/modules/work-area/workspace/workspace.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { WorkspaceTitleComponent} from './workspace-title/workspace-title.component';
 import { CommonModule } from '@angular/common';
 import { ListComponent} from './list/list.component';
@@ -16,6 +16,13 @@ import { ListComponent} from './list/list.component';
 })
 export class WorkspaceComponent {
   @Input() backgroundColor: string = '#f1f2f6';
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    // Manually trigger change detection
+    this.cdr.detectChanges();
+  }
 
   ngOnChanges() {
     console.log('Background color changed to:', this.backgroundColor);
