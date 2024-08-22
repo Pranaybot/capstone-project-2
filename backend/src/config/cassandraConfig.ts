d95485fe8810a5d2e56c72661ae2d15ce6729365
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-22 16:27:24 -0400

    replaced client id and secret variables with application token JSON variable string in env file

--- backend/src/config/cassandraConfig.ts
+++ backend/src/config/cassandraConfig.ts
@@ -7,8 +7,8 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 // Define the path where the bundle is located
 const bundlePath = path.join(__dirname, 'secure-connect-taskerr-db.zip');
 
-const username = process.env["ASTRA_DB_USERNAME"] || '';
-const password = process.env["ASTRA_DB_APPLICATION_TOKEN"] || ''; // Ensure password is included
+// Parse the JSON string back into an object
+const applicationToken = JSON.parse(process.env["ASTRA_DB_APPLICATION_TOKEN_JSON"]!) || '';
 const keyspace = process.env["ASTRA_KEYSPACE_NAME"] || '';
 
 // Config object including the path to the local secure connect bundle
@@ -16,9 +16,10 @@ const config = {
     cloud: { secureConnectBundle: bundlePath },
     keyspace: keyspace,
     credentials: {
-        username: username,
-        password: password
-    }
+        username: applicationToken["clientId"],
+        password: applicationToken["secret"]
+    },
+    token: applicationToken["token"]
 };
 
 export default config;
