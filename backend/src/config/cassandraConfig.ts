8568dab39d93259c7ee4d6d98475796d7f3e20f7
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-22 16:08:03 -0400

    removed client id and secret from env file

--- backend/src/config/cassandraConfig.ts
+++ backend/src/config/cassandraConfig.ts
@@ -7,18 +7,14 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 // Define the path where the bundle is located
 const bundlePath = path.join(__dirname, 'secure-connect-taskerr-db.zip');
 
-const username = process.env["ASTRA_DB_APPLICATION_TOKEN"] || '';
-const password = process.env["ASTRA_DB_PASSWORD"] || ''; // Ensure password is included
+const username = process.env["ASTRA_DB_USERNAME"] || '';
+const password = process.env["ASTRA_DB_APPLICATION_TOKEN"] || ''; // Ensure password is included
 const keyspace = process.env["ASTRA_KEYSPACE_NAME"] || '';
-const clientId = process.env["ASTRA_CLIENT_ID"] || '';
-const clientSecret = process.env["ASTRA_CLIENT_SECRET"] || '';
 
 // Config object including the path to the local secure connect bundle
 const config = {
     cloud: { secureConnectBundle: bundlePath },
     keyspace: keyspace,
-    clientId: clientId,
-    clientSecret: clientSecret,
     credentials: {
         username: username,
         password: password
