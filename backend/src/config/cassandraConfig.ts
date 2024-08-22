98935d7779ea025c0f9df80cfc3edf284f3c77fc
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-22 15:48:24 -0400

    removed bundleDownloader file and removedbundle url from env file

--- backend/src/config/cassandraConfig.ts
+++ backend/src/config/cassandraConfig.ts
@@ -1,25 +1,19 @@
 const dotenv = require('dotenv');
 const path = require('path');
-import { downloadBundleIfNeeded } from "../utils/bundleDownloader";
 
 // Configure dotenv to load environment variables from the .env file
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-const bundleUrl = process.env["SECURE_CONNECT_BUNDLE_URL"] || '';
+// Define the path where the bundle is located
+const bundlePath = path.join(__dirname, 'secure-connect-taskerr-db.zip');
+
 const username = process.env["ASTRA_DB_APPLICATION_TOKEN"] || '';
 const password = process.env["ASTRA_DB_PASSWORD"] || ''; // Ensure password is included
 const keyspace = process.env["ASTRA_KEYSPACE_NAME"] || '';
 const clientId = process.env["ASTRA_CLIENT_ID"] || '';
 const clientSecret = process.env["ASTRA_CLIENT_SECRET"] || '';
 
-// Call the function to ensure the bundle is downloaded
-downloadBundleIfNeeded(bundleUrl).catch(err => {
-    console.error('Error downloading secure connect bundle:', err);
-});
-
-// Define the path where the bundle should be saved
-const bundlePath = path.join(__dirname, 'secure-connect-taskerr-db.zip');
-
+// Config object including the path to the local secure connect bundle
 const config = {
     cloud: { secureConnectBundle: bundlePath },
     keyspace: keyspace,
