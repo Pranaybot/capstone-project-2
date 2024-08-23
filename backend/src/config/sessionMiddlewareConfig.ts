e0c6712c6b76d4961be31770f9ee165f912118a4
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-23 10:25:43 -0400

    commented old session setup and added new setup in sessionMiddlewareConfig.ts

--- backend/src/config/sessionMiddlewareConfig.ts
+++ backend/src/config/sessionMiddlewareConfig.ts
@@ -1,14 +1,33 @@
 import { Application } from 'express';
-import session from 'express-session';
+const session = require('express-session');
+const redis = require('redis');
+const client = redis.createClient();
+const redisStore = require('connect-redis')(session);
 
 
 export function configureSessionMiddlware(app: Application) {
-    // Session middleware setup
+
+    // New Session middleware setup
+    app.use(session({
+      store: new redisStore({ client: client }),
+      secret: 'topsecret~!@#$%^&*',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: true,
+        secure: false,
+        httpOnly: false,
+        maxAge: 1000 * 60 * 10 // 10 minutes
+      }
+    }))
+    
+    /* Original Session middleware setup
     app.use(session({
         secret: 'a4g_gdfg-$5',
         resave: false,
         saveUninitialized: false,
         cookie: { secure: false } // Set to true if using HTTPS
     }))
+    */
 }
   
