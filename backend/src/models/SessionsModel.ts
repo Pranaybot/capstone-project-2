fdded798cf5e9079890c1aaaf03c2c574cb8e354
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-08-23 10:28:47 -0400

    commented SessionsModel.ts

--- backend/src/models/SessionsModel.ts
+++ backend/src/models/SessionsModel.ts
@@ -1,7 +1,9 @@
 // Define the schema for the sessions table
+/*
 export const createSessionsTableQuery = `
   CREATE TABLE IF NOT EXISTS sessions (
     session_id text PRIMARY KEY,
     session_data text
   )
 `;
+*/
