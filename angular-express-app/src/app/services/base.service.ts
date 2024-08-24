e721a16d5052ac58ae210b204c3ad8941dc1492b
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-24 17:36:33 -0400

    updated some important files

--- angular-express-app/src/app/services/base.service.ts
+++ angular-express-app/src/app/services/base.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod'; // Adjust the path as necessary
 
 export class BaseService {
-  protected apiUrl: string = '/api';
+  protected apiUrl: string = environment.apiUrl;
 
   constructor(protected http: HttpClient) {}
 
---
+++ angular-express-app/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://capstone-project-2-i62m.onrender.com'
+};
+  
