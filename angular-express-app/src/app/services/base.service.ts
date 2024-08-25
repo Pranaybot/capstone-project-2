3324377a4b9cee313e79423b216a9bd7ce05d777
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-25 11:52:38 -0400

    added baseHref to angular.json file

--- angular-express-app/src/app/services/base.service.ts
+++ angular-express-app/src/app/services/base.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
+import { environment } from "../../environments/environment.prod";
 
 export class BaseService {
-  protected apiUrl: string = "/api";
+  protected apiUrl: string = `${environment.apiUrl}/api`;
 
   constructor(protected http: HttpClient) {}
 
