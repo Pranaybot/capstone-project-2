57fd53197411d0a250b209cb1c7a3842b69d25cb
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-25 10:19:47 -0400

    removed path import from appMiddleware file

--- backend/src/utils/route_handlers/appMiddleware.ts
+++ backend/src/utils/route_handlers/appMiddleware.ts
@@ -3,21 +3,19 @@
 import { Application } from 'express';
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-
 
 function setUp(app: Application) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
   
-// Serve static files from Angular build directory
-app.use(express.static(path.join(__dirname, '../../../angular-express-app/dist/angular-express-app/browser')));
+  // Serve static files from Angular build directory
+  app.use(express.static('../../../angular-express-app/dist/angular-express-app/browser'));
 
-// Define a catch-all route to serve the Angular application
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../../angular-express-app/dist/angular-express-app/browser/index.html'));
-});
+  // Define a catch-all route to serve the Angular application
+  app.get('*', (_req, res) => {
+    res.sendFile('../../../angular-express-app/dist/angular-express-app/browser/index.html');
+  });
 }
 
 module.exports = { setUp };
