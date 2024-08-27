29c9ada3292678fb9f21ffd860ffb7177a5a7856
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-27 19:49:13 -0400

    changed env variable of port to correct value

--- backend/src/config/sessionMiddlewareConfig.ts
+++ backend/src/config/sessionMiddlewareConfig.ts
@@ -10,7 +10,7 @@ function initializeRedisStore() {
     password: process.env["REDIS_PASSWORD"],
     socket: {
         host: process.env["REDIS_HOST"],
-        port: process.env["REDIS_HOST"]
+        port: process.env["REDIS_PORT"]
     }
 });
 
