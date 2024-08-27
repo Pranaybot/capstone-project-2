0f32f011646e31feca05a2edd9837ca76cf0fb74
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-08-27 15:35:57 -0400

    added cors import

--- backend/src/utils/route_handlers/appMiddleware.ts
+++ backend/src/utils/route_handlers/appMiddleware.ts
@@ -2,11 +2,13 @@
 // appMiddleware.ts
 import { Application, Request, Response } from 'express';
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 function setUp(app: Application) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 
   app.use(express.static(path.join(__dirname, 
     '../../../angular-dist/angular-express-app/browser/browser')));
