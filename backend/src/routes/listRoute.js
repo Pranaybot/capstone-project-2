26ee6f335ef25006b344cdbe622097e5aeed497f
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-09-09 15:19:19 -0400

    Update listRoute.js

--- backend/src/routes/listRoute.js
+++ backend/src/routes/listRoute.js
@@ -1,46 +1,46 @@
-const { Router } = require('express');
-const { ListController } = require('../controllers/listController');
-const { v4: uuidv4 } = require('uuid'); // Import UUID generator
+import { Router, Request, Response } from 'express';
+import ListController from '../controllers/listController';
+import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
 
 const router = Router();
 const listController = new ListController();
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req: Request, res: Response) => {
   try {
     const lists = await listController.findAllLists();
     return res.json(lists);
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 });
 
 // Handle form submission and validation
-router.post('/add_list', async (req, res) => {
+router.post('/add_list', async (req: Request, res: Response) => {
   try {
     const id = uuidv4(); // Generate a new UUID for the list
     const { name, cards } = req.body;
     await listController.createList(id, name, cards);
     return res.json({ id, name, cards });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 });
 
-router.post('/update_list', async (req, res) => {
+router.post('/update_list', async (req: Request, res: Response) => {
   try {
     const { id, name } = req.body;
     await listController.updateList(id, name);
     const list = await listController.findList(id);
     
     return res.json({ list });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req: Request, res: Response) => {
   try {
-    const listId = req.params['id'];
+    const listId: string = req.params['id'] || '';
 
     if (typeof listId !== 'string') {
       return res.status(400).json({ error: 'Invalid list ID' });
@@ -48,9 +48,9 @@ router.delete('/:id', async (req, res) => {
 
     await listController.deleteList(listId);
     return res.json({ message: 'List deleted successfully' });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 });
 
-module.exports = router;
+export default router;
