953281e0ec6691edeee94cd136ad98a54e29711c
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-09-10 13:59:31 -0400

    added delete user account query in user.ts

--- backend/src/utils/queries/user.ts
+++ backend/src/utils/queries/user.ts
@@ -1,10 +1,12 @@
 const SELECT_USER_BY_USERNAME = `SELECT * FROM users WHERE userId = ? ALLOW FILTERING`;
 const INSERT_USER = `INSERT INTO users (id, firstName, lastName, userId, password) VALUES (?, ?, ?, ?, ?)`;
 const UPDATE_USER_PASSWORD = `UPDATE users SET password = ? WHERE id = ?`;
+const DELETE_USER_ACCOUNT = `DELETE FROM users WHERE id = ?`;
 
 export default {
   SELECT_USER_BY_USERNAME,
   INSERT_USER,
-  UPDATE_USER_PASSWORD
+  UPDATE_USER_PASSWORD,
+  DELETE_USER_ACCOUNT
 };
 
