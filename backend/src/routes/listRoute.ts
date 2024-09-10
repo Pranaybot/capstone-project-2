4330c33a2ac35b491208c0ea1c1b03539ad77bea
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-09-10 16:13:24 -0400

    fixed issue with cards argument

--- backend/src/routes/listRoute.ts
+++ backend/src/routes/listRoute.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
 import ListController from '../controllers/listController';
+import CardController from '../controllers/cardController';
 import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
 
 const router = Router();
 const listController = new ListController();
+const cardController = new CardController();
 
 router.get('/', async (_req: Request, res: Response) => {
   try {
@@ -21,8 +23,8 @@ router.post('/add_list', async (req: Request, res: Response) => {
     const cardId = uuidv4(); // Generate a new UUID for the list
     const { name, username, title, description, activity } = req.body;
     const cards = await cardController.createCard(cardId, username, title, description, activity);
-    await listController.createList(id, name, [cards]);
-    return res.json({ id, name, [cards] });
+    await listController.createList(listId, name, [cards]);
+    return res.json({ listId, name, cards: [cards] });
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
