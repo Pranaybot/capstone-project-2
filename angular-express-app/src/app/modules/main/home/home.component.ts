8d946cf382724647378cbdde92cd4a74c1267ae8
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-09-12 13:35:55 -0400

    removed code for updating theme in home.component.ts

--- angular-express-app/src/app/modules/main/home/home.component.ts
+++ angular-express-app/src/app/modules/main/home/home.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseComponent } from "../../layout/base/base.component";
-import { ThemeService } from '../../../services/settings/theme.service';
 
 @Component({
   selector: 'app-home',
@@ -13,21 +12,4 @@ import { ThemeService } from '../../../services/settings/theme.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  currentTheme: string = 'light';
-
-  constructor(private themeService: ThemeService) {}
-
-  ngOnInit() {
-    const savedTheme = this.themeService.getSavedTheme();
-    if (savedTheme) {
-      this.currentTheme = savedTheme;
-      this.themeService.applyTheme(savedTheme);
-    }
-  }
-
-  changeTheme(theme: string) {
-    this.currentTheme = theme;
-    this.themeService.applyTheme(theme);
-  }
-}
+export class HomeComponent {}
