0ffe2f1b604435b3d54095435657cf1b7f54ce40
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-09-13 15:09:30 -0400

    made changes to login/logout functionality

--- angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
+++ angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../services/forms/user.service';
 import { ThemeService } from '../../../services/settings/theme.service';
@@ -10,9 +10,21 @@ import { ThemeService } from '../../../services/settings/theme.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  isHome: boolean = true;
 
   constructor(public userService: UserService, public themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.themeService.isLoggedIn$.subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+
+    this.themeService.isHome$.subscribe(home => {
+      this.isHome = home;
+    });
+  }
 
   onLogout() {
     this.userService.logout();
--- angular-express-app/src/app/services/settings/theme.service.ts
+++ angular-express-app/src/app/services/settings/theme.service.ts
@@ -1,18 +1,30 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { CookieService } from "ngx-cookie-service";
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
   private isBrowser: boolean;
+  private isLoggedInSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.getIsLoggedInFromCookies());
+  private isHomeSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.getIsHomeFromCookies());
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
     private cookieService: CookieService
   ) {
     this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  // Add these methods to retrieve logged in state and home state from cookies
+  private getIsLoggedInFromCookies(): boolean {
+    return this.isBrowser ? (this.cookieService.get('isLoggedIn') === 'true') : false;
+  }
+  
+  private getIsHomeFromCookies(): boolean {
+    return this.isBrowser ? (this.cookieService.get('isHome') === 'true') : false;
   }
 
   applyTheme(theme: string) {
@@ -39,21 +51,25 @@ export class ThemeService {
   setLoggedInState(isLoggedIn: boolean) {
     if (this.isBrowser) {
       this.cookieService.set('isLoggedIn', String(isLoggedIn));
+      this.isLoggedInSubject.next(isLoggedIn); // Update BehaviorSubject
     }
   }
 
-  get isLoggedIn$(): boolean | null {
-    return this.isBrowser ? (this.cookieService.get('isLoggedIn') === 'true') : null;
+  // Observable for isLoggedIn
+  get isLoggedIn$(): Observable<boolean> {
+    return this.isLoggedInSubject.asObservable();
   }
 
   setHomeState(isHome: boolean) {
     if (this.isBrowser) {
       this.cookieService.set('isHome', String(isHome));
+      this.isHomeSubject.next(isHome); // Update BehaviorSubject
     }
   }
 
-  get isHome$(): boolean | null {
-    return this.isBrowser ? (this.cookieService.get('isHome') === 'true') : null;
+  // Observable for isHome
+  get isHome$(): Observable<boolean> {
+    return this.isHomeSubject.asObservable();
   }
 
   setUserId(id: string) {
