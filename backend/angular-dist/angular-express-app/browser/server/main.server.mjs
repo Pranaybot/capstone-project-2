04e7d34a633bbdb21e40087d7a2311a44999000c
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-09-14 09:39:42 -0400

    added new angular-dist directory to backend

--- backend/angular-dist/angular-express-app/browser/server/main.server.mjs
+++ backend/angular-dist/angular-express-app/browser/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NLE5JXH2.mjs";import"./chunk-P4Q3GMB3.mjs";import"./chunk-3FLROKJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7BXRGWWH.mjs";import"./chunk-P4Q3GMB3.mjs";import"./chunk-3FLROKJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
