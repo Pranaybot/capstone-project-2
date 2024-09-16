28359475a115e2089eb6354b89f331d87c9e9e03
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2024-09-15 23:30:48 -0400

    added new dist folder

--- backend/angular-dist/angular-express-app/browser/server/main.server.mjs
+++ backend/angular-dist/angular-express-app/browser/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E5WMF5SX.mjs";import"./chunk-P4Q3GMB3.mjs";import"./chunk-3FLROKJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SMXXG7V7.mjs";import"./chunk-P4Q3GMB3.mjs";import"./chunk-3FLROKJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
