2769a7d91f02a9f981a079c972b14bca804ab6c9
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-09-18 13:19:51 -0400

    added AuthService import to navbar.component.ts

--- angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
+++ angular-express-app/src/app/modules/layout/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../services/forms/user.service';
 import { Observable } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
