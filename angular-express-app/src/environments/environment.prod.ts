a9f44385953536e3421e3deece077be41bbc831f
Pranaybot <30740547+Pranaybot@users.noreply.github.com>
2024-09-18 12:59:24 -0400

    Update environment.prod.ts

--- angular-express-app/src/environments/environment.prod.ts
+++ angular-express-app/src/environments/environment.prod.ts
@@ -3,10 +3,5 @@ import { domain, clientId } from '../../auth_conifg.json';
 export const environment = {
     production: true,
     apiUrl: 'https://capstone-project-2-i62m.onrender.com'
-    auth: {
-      domain, 
-      clientId,
-      redirectUri: window.location.origin
-    }
 };
   
